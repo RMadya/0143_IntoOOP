c9e254b2b31c9cfe02b8d13cbee399e0f2c2b640
RMadya <kereyoben75@gmail.com>
2024-04-22 16:33:53 +0700

    cout cin PL

--- Class_BangunDatar_143/Class_BangunDatar_143.cpp
+++ Class_BangunDatar_143/Class_BangunDatar_143.cpp
@@ -12,4 +12,9 @@ public:
 
 	void input() { // methode input persegi panjang
 
-		
+		cout << "Masukkan Panjangnya = ";
+		cin >> panjang;
+		cout << "Masukkan Lebarnya = "; // step 1
+		cin >> Lebar;
+	}
+	
